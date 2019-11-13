513d9cdcc8df7e89d6ec273335cad34ba4a5abf4
Simon <baeumer@pm.me>
2019-11-13 14:21:17 +0100

    add version command

--- cmd/root.go
+++ cmd/root.go
@@ -9,6 +9,7 @@ import (
 
 var cfgFile string
 var Debug bool
+var version string
 
 const ConfigFile = ".monorepo-operator.yml"
 
@@ -21,7 +22,9 @@ var rootCmd = &cobra.Command{
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func Execute(v string) {
+	version = v
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
