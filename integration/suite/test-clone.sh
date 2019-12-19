9a7cbd0cf425afc7b071af2448650d6a38518e60
Simon <baeumer@pm.me>
2019-12-19 15:47:41 +0100

    Add clone test

--- integration/suite/test-clone.sh
+++ integration/suite/test-clone.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+set -e
+
+rm -rf .git/.subtree-repos
+
+monorepo-operator clone
+
+echo "### CHECKING CLONE RESULTS"
+ls -l .git/.subtree-repos/ | awk '{print $9}' | diff ./../suite/test-clone-results.txt -
