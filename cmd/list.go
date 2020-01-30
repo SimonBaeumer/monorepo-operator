406e1257193067934c7fce0150670c2cc9af7416
Simon <baeumer@pm.me>
2020-01-30 11:53:45 +0100

    Add list command

---
+++ cmd/list.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"github.com/SimonBaeumer/monorepo-operator/operator"
+	"github.com/olekukonko/tablewriter"
+	"github.com/spf13/cobra"
+	"log"
+	"os"
+)
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "Lists all projects",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		m, err := operator.NewMonoRepo(cfgFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		header := []string{"name", "path", "git-url"}
+		list := [][]string{}
+		for _, p := range m.Projects {
+			list = append([][]string{[]string{p.Name, p.Path, p.GitUrl}})
+		}
+
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader(header)
+		table.AppendBulk(list)
+		table.Render()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
